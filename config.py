99897b6b1a03f8687ccd4c9e9ad3be2fe19ca6b0
Nachiket <nachiket.tanksale@gmail.com>
2021-10-25 23:52:47 +0530

    Added code to read config file using complete path

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 import configparser
+import os
 
 def get_config():
     parser = configparser.ConfigParser()
-    parser.read('config.txt')
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        'config.txt')
+    parser.read(path)
     return parser['DEFAULT']
 
 def set_config(configs):
     assert(type(configs) == dict)
     parser = configparser.ConfigParser()
-    parser.read('config.txt')
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        'config.txt')
+    parser.read(path)
     for key in configs.keys():
         if key in parser['DEFAULT']:
             parser.set('DEFAULT', key, str(configs[key]))
 
-    parser.write(open('config.txt', 'w'))
+    parser.write(open(path, 'w'))
